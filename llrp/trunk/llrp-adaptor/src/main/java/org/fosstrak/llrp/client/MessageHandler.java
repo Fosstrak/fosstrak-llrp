71e79c6e606625ff0326893ebfb6ede91a7c9266
sawielan <sawielan@b63d296a-7662-4df4-3098-00f9c2c9103a>
2009-05-19 19:12:12 +0000

    - update documentation and javadoc.

--- llrp/trunk/llrp-adaptor/src/main/java/org/fosstrak/llrp/client/MessageHandler.java
+++ llrp/trunk/llrp-adaptor/src/main/java/org/fosstrak/llrp/client/MessageHandler.java
@@ -1,7 +1,24 @@
 package org.fosstrak.llrp.client;
 
+import org.fosstrak.llrp.adaptor.AdaptorManagement;
 import org.llrp.ltk.types.LLRPMessage;
 
+/**
+ * All processes that want to receive messages from the {@link AdaptorManagement} 
+ * need to implement this interface. Upon arrival of a new LLRP message, the 
+ * {@link AdaptorManagement} invokes the handle method on the registered 
+ * handlers.
+ * @author sawielan
+ *
+ */
 public interface MessageHandler {
+	
+	/**
+	 * This method is invoked from the adapter management whenever a new LLRP 
+	 * message arrives on an attached reader.
+	 * @param adaptorName the name of the adapter where the reader belongs to. 
+	 * @param readerName the name of the receiving reader.
+	 * @param message the LLRP message.
+	 */
 	public void handle(String adaptorName, String readerName, LLRPMessage message);
 }
